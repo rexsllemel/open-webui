280637c5e731d87f4d359808d548a05886fb5cb6
rexsllemel <rexs.llemel333@gmail.com>
2026-08-21 10:01:04 +0800

    fix: update application name and favicon URL for iPlast AI

--- backend/open_webui/env.py
+++ backend/open_webui/env.py
@@ -127,11 +127,11 @@ if 'cuda_error' in locals():
 
 SRC_LOG_LEVELS = {}  # Legacy variable, do not remove
 
-WEBUI_NAME = os.environ.get('WEBUI_NAME', 'Open WebUI')
-if WEBUI_NAME != 'Open WebUI':
-    WEBUI_NAME += ' (Open WebUI)'
+WEBUI_NAME = os.environ.get('WEBUI_NAME', 'iPlast AI')
+if WEBUI_NAME != 'iPlast AI':
+    WEBUI_NAME += ' (iPlast AI)'
 
-WEBUI_FAVICON_URL = 'https://openwebui.com/favicon.png'
+WEBUI_FAVICON_URL = '/static/favicon.png'
 
 TRUSTED_SIGNATURE_KEY = os.environ.get('TRUSTED_SIGNATURE_KEY', '')
 
--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-image_name="open-webui"
-container_name="open-webui"
-host_port=3000
+image_name="iplast-webui"
+container_name="iplast-webui"
+host_port=3112
 container_port=8080
 
 docker build -t "$image_name" .
